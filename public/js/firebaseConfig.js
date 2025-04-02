12f31894c9e5760fe53ee2bb8f3215299ca78d88
Darktrace <darktrace@gmail.com>
2025-04-02 12:25:05 +0100

    feat: add_btns

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCaiODVNHbWIF1K0Kk08Udd2andVi3heOc",
+    authDomain: "budgetapp-b592a.firebaseapp.com",
+    projectId: "budgetapp-b592a",
+    storageBucket: "budgetapp-b592a.firebasestorage.app",
+    messagingSenderId: "899009356621",
+    appId: "1:899009356621:web:abad8af37010fdc9f04d04",
+    measurementId: "G-PZPY6HZLWF"
+};
